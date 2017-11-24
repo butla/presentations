35d2793f3cc9ea6a1c99bd970a35bccd0102695e
Michal Bultrowicz <michalbultrowicz@gmail.com>
2017-11-24 21:36:16 +0100

    cli as ide: initial mindmap

---
+++ python/cli-as-ide/terminal_wystarczy-praca_z_projektami_pythonowymi_z_poziomu_konsoli.mm
@@ -0,0 +1,43 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1511017570130" ID="ID_59680098" MODIFIED="1511021286344" TEXT="Praca w konsoli na przyk&#x142;adzie faktycznego commitu">
+<node CREATED="1511019466467" ID="ID_1059965362" MODIFIED="1511019487308" POSITION="right" TEXT="Robota do zrobienia">
+<node CREATED="1511021447342" ID="ID_1560050008" MODIFIED="1511021458392" TEXT="Jak si&#x119; zdecydowa&#x107;, co to ma by&#x107;?">
+<node CREATED="1511021709913" ID="ID_352744740" MODIFIED="1511021715736" TEXT="Przejrzyj wszystkie notki"/>
+</node>
+</node>
+<node CREATED="1511019490393" ID="ID_1916535455" MODIFIED="1511021086595" POSITION="left" TEXT="Narz&#x119;dzia do pokazania">
+<node CREATED="1511021089898" ID="ID_1201074486" MODIFIED="1511021099096" TEXT="Tmux"/>
+<node CREATED="1511021105315" ID="ID_16674722" MODIFIED="1511021107147" TEXT="Ranger"/>
+<node CREATED="1511021108076" ID="ID_1666115729" MODIFIED="1511021109379" TEXT="VIM"/>
+<node CREATED="1511021110374" ID="ID_642236478" MODIFIED="1511021113746" TEXT="tox"/>
+<node CREATED="1511021114442" ID="ID_1583638695" MODIFIED="1511021116505" TEXT="pytest"/>
+<node CREATED="1511021125501" ID="ID_1834277194" MODIFIED="1511021127452" TEXT="pudb"/>
+<node CREATED="1511021128673" ID="ID_1186835724" MODIFIED="1511021132888" TEXT="pgcli">
+<node CREATED="1511021137922" ID="ID_792676511" MODIFIED="1511021152860" TEXT="mo&#x17c;na te&#x17c; wspomnie&#x107;, &#x17c;e s&#x105; takie narz&#x119;dzia do innych baz"/>
+</node>
+</node>
+<node CREATED="1511021055611" ID="ID_1622279434" MODIFIED="1511021515404" POSITION="right" TEXT="zamie&#x15b;&#x107; gdzie&#x15b; t&#x119; map&#x119; i configi do wszystkich narz&#x119;dzi"/>
+<node CREATED="1511021291938" ID="ID_823891076" MODIFIED="1511021763405" POSITION="left" TEXT="narz&#x119;dzia do zrobienia tej prezentacji">
+<node CREATED="1511021307328" ID="ID_1494907374" MODIFIED="1511021318112" TEXT="pewnie co&#x15b; w stylu toilet i python"/>
+<node CREATED="1511021323485" ID="ID_1814247079" MODIFIED="1511021329594" TEXT="freemind">
+<node CREATED="1511021343079" ID="ID_1586476694" MODIFIED="1511021357653" TEXT="&#x142;adnie mo&#x17c;na go u&#x17c;ywa&#x107; z klawiatury"/>
+<node CREATED="1511021359128" ID="ID_1426225525" MODIFIED="1511021388067" TEXT="&#x142;atwiej mo&#x17c;na go odpali&#x107; ni&#x17c; xmind"/>
+<node CREATED="1511021388870" ID="ID_52605028" MODIFIED="1511021396307" TEXT="brzydszy ni&#x17c; xmind"/>
+<node CREATED="1511021396932" ID="ID_28683638" MODIFIED="1511021414430" TEXT="zostawia format plik&#xf3;w kt&#xf3;ry lepiej nadaje si&#x119; do Gita"/>
+</node>
+<node CREATED="1511021768502" ID="ID_1975184325" MODIFIED="1511021808303" TEXT="kiedy&#x15b; my&#x15b;la&#x142;em, &#x17c;e mind mapy s&#x105; zbyt czasoch&#x142;onne i wcale nie s&#x105; lepsze ni&#x17c; od razu zacz&#x119;cie szkicowania slajd&#xf3;w"/>
+</node>
+<node CREATED="1511021552951" ID="ID_554030902" MODIFIED="1511021557483" POSITION="right" TEXT="Czemu konsola?">
+<node CREATED="1511021559862" ID="ID_496616300" MODIFIED="1511021572604" TEXT="Mo&#x17c;esz sam wszystko &#x142;atwo &#x142;&#x105;czy&#x107; ze sob&#x105;"/>
+<node CREATED="1511021587984" ID="ID_1066472959" MODIFIED="1511021611656" TEXT="Nabywasz umiej&#x119;tno&#x15b;ci uniwersalne, w przeciwie&#x144;stwie do zamykania si&#x119; w jakim&#x15b; IDE"/>
+<node CREATED="1511021616279" ID="ID_1550495647" MODIFIED="1511021630444" TEXT="Rozumiesz, co tak naprawd&#x119; si&#x119; dzieje, kiedy robisz buildy itp"/>
+<node CREATED="1511021631797" ID="ID_1907068219" MODIFIED="1511021639848" TEXT="Nie nadaje si&#x119; do wszystkiego">
+<node CREATED="1511021641922" ID="ID_1895422566" MODIFIED="1511021646631" TEXT="Wizualny diff"/>
+<node CREATED="1511021647652" ID="ID_1079713419" MODIFIED="1511021651903" TEXT="Mind mapa"/>
+<node CREATED="1511021653638" ID="ID_1756550722" MODIFIED="1511021656736" TEXT="Przegl&#x105;darka"/>
+<node CREATED="1511021657201" ID="ID_78225732" MODIFIED="1511021659526" TEXT="Filmy"/>
+</node>
+</node>
+</node>
+</map>

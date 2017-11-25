e4dfbe1d321bc2bfb288e66271481f3709ab69fb
Michal Bultrowicz <michalbultrowicz@gmail.com>
2017-11-25 19:24:13 +0100

    cli as ide: Rozwój mind mapy postępuje...

--- python/cli-as-ide/terminal_wystarczy-praca_z_projektami_pythonowymi_z_poziomu_konsoli.mm
+++ python/cli-as-ide/terminal_wystarczy-praca_z_projektami_pythonowymi_z_poziomu_konsoli.mm
@@ -5,8 +5,16 @@
 <node CREATED="1511021447342" ID="ID_1560050008" MODIFIED="1511021458392" TEXT="Jak si&#x119; zdecydowa&#x107;, co to ma by&#x107;?">
 <node CREATED="1511021709913" ID="ID_352744740" MODIFIED="1511021715736" TEXT="Przejrzyj wszystkie notki"/>
 </node>
+<node CREATED="1511631372176" ID="ID_1007858332" MODIFIED="1511631393108" TEXT="Odznaczaj ogarni&#x119;te elementy tego drzewa">
+<node CREATED="1511631394681" ID="ID_1904463289" MODIFIED="1511631404548" TEXT="Ptaszek na zawarte"/>
+<node CREATED="1511631406489" ID="ID_1477009413" MODIFIED="1511631419501" TEXT="Krzy&#x17c;yk na odrzucone"/>
 </node>
-<node CREATED="1511019490393" ID="ID_1916535455" MODIFIED="1511021086595" POSITION="left" TEXT="Narz&#x119;dzia do pokazania">
+<node CREATED="1511021055611" ID="ID_1622279434" MODIFIED="1511021515404" TEXT="zamie&#x15b;&#x107; gdzie&#x15b; t&#x119; map&#x119; i configi do wszystkich narz&#x119;dzi">
+<node CREATED="1511633779484" ID="ID_1154819249" MODIFIED="1511633793920" TEXT="Mo&#x17c;e te&#x17c; instrukcje instalacji?">
+<node CREATED="1511633794756" ID="ID_1159219490" MODIFIED="1511633802616" TEXT="pip3 install --user neovim"/>
+</node>
+</node>
+<node CREATED="1511019490393" ID="ID_1916535455" MODIFIED="1511021086595" TEXT="Narz&#x119;dzia do pokazania">
 <node CREATED="1511021089898" ID="ID_1201074486" MODIFIED="1511021099096" TEXT="Tmux"/>
 <node CREATED="1511021105315" ID="ID_16674722" MODIFIED="1511021107147" TEXT="Ranger"/>
 <node CREATED="1511021108076" ID="ID_1666115729" MODIFIED="1511021109379" TEXT="VIM"/>
@@ -16,8 +24,9 @@
 <node CREATED="1511021128673" ID="ID_1186835724" MODIFIED="1511021132888" TEXT="pgcli">
 <node CREATED="1511021137922" ID="ID_792676511" MODIFIED="1511021152860" TEXT="mo&#x17c;na te&#x17c; wspomnie&#x107;, &#x17c;e s&#x105; takie narz&#x119;dzia do innych baz"/>
 </node>
+<node CREATED="1511631630226" ID="ID_1657220637" MODIFIED="1511631633861" TEXT="meld"/>
 </node>
-<node CREATED="1511021055611" ID="ID_1622279434" MODIFIED="1511021515404" POSITION="right" TEXT="zamie&#x15b;&#x107; gdzie&#x15b; t&#x119; map&#x119; i configi do wszystkich narz&#x119;dzi"/>
+</node>
 <node CREATED="1511021291938" ID="ID_823891076" MODIFIED="1511021763405" POSITION="left" TEXT="narz&#x119;dzia do zrobienia tej prezentacji">
 <node CREATED="1511021307328" ID="ID_1494907374" MODIFIED="1511021318112" TEXT="pewnie co&#x15b; w stylu toilet i python"/>
 <node CREATED="1511021323485" ID="ID_1814247079" MODIFIED="1511021329594" TEXT="freemind">
@@ -29,14 +38,86 @@
 <node CREATED="1511021768502" ID="ID_1975184325" MODIFIED="1511021808303" TEXT="kiedy&#x15b; my&#x15b;la&#x142;em, &#x17c;e mind mapy s&#x105; zbyt czasoch&#x142;onne i wcale nie s&#x105; lepsze ni&#x17c; od razu zacz&#x119;cie szkicowania slajd&#xf3;w"/>
 </node>
 <node CREATED="1511021552951" ID="ID_554030902" MODIFIED="1511021557483" POSITION="right" TEXT="Czemu konsola?">
-<node CREATED="1511021559862" ID="ID_496616300" MODIFIED="1511021572604" TEXT="Mo&#x17c;esz sam wszystko &#x142;atwo &#x142;&#x105;czy&#x107; ze sob&#x105;"/>
+<node CREATED="1511021559862" ID="ID_496616300" MODIFIED="1511632054676" TEXT="Mo&#x17c;esz sam wszystko &#x142;atwo &#x142;&#x105;czy&#x107; ze sob&#x105; jak ci si&#x119; podoba."/>
 <node CREATED="1511021587984" ID="ID_1066472959" MODIFIED="1511021611656" TEXT="Nabywasz umiej&#x119;tno&#x15b;ci uniwersalne, w przeciwie&#x144;stwie do zamykania si&#x119; w jakim&#x15b; IDE"/>
+<node CREATED="1511632064688" ID="ID_329801130" MODIFIED="1511632105763" TEXT="Nie jeste&#x15b; zdany na producent&#xf3;w IDE"/>
 <node CREATED="1511021616279" ID="ID_1550495647" MODIFIED="1511021630444" TEXT="Rozumiesz, co tak naprawd&#x119; si&#x119; dzieje, kiedy robisz buildy itp"/>
-<node CREATED="1511021631797" ID="ID_1907068219" MODIFIED="1511021639848" TEXT="Nie nadaje si&#x119; do wszystkiego">
-<node CREATED="1511021641922" ID="ID_1895422566" MODIFIED="1511021646631" TEXT="Wizualny diff"/>
-<node CREATED="1511021647652" ID="ID_1079713419" MODIFIED="1511021651903" TEXT="Mind mapa"/>
+<node CREATED="1511631895105" ID="ID_1811894841" MODIFIED="1511631983140" TEXT="U&#x17c;ywa&#x142;em i tak wielu program&#xf3;w z konsoli (ansible, terraform, ptpython). Teraz mam &#x15b;rodowisko zintegrowane ze wszystkim."/>
+<node CREATED="1511631990137" ID="ID_1393844905" MODIFIED="1511632028252" TEXT="Nie chcia&#x142;o mi si&#x119; czeka&#x107; na wstanie IDE jak edytowa&#x142;em ma&#x142;e pliki, albo jak robi&#x142;em ma&#x142;e zmiany w kilku projektach."/>
+<node CREATED="1511021631797" ID="ID_1907068219" MODIFIED="1511630793629" TEXT="Nie nadaje si&#x119; do wszystkiego">
 <node CREATED="1511021653638" ID="ID_1756550722" MODIFIED="1511021656736" TEXT="Przegl&#x105;darka"/>
+<node CREATED="1511021647652" ID="ID_1079713419" MODIFIED="1511021651903" TEXT="Mind mapa"/>
 <node CREATED="1511021657201" ID="ID_78225732" MODIFIED="1511021659526" TEXT="Filmy"/>
+<node CREATED="1511021641922" ID="ID_1895422566" MODIFIED="1511630976223" TEXT="Wizualny diff"/>
+</node>
+</node>
+<node CREATED="1511631172525" ID="ID_1115458077" MODIFIED="1511631184079" POSITION="right" TEXT="Plan prezentacji">
+<node CREATED="1511631450176" ID="ID_1946710202" MODIFIED="1511631463548" TEXT="Powiedz czemu w og&#xf3;le siedzie&#x107; w konsoli"/>
+<node CREATED="1511631196053" ID="ID_474511385" MODIFIED="1511631243050" TEXT="VIM jest spoko do pisania kodu">
+<node CREATED="1511631828178" ID="ID_1671329438" MODIFIED="1511631846396" TEXT="B&#x119;dzie kr&#xf3;tko o nim, bo b&#x119;dzie w innej prezentacji, bo za du&#x17c;y"/>
+<node CREATED="1511631247303" ID="ID_1832783099" MODIFIED="1511631258658" TEXT="Podpowiadanie"/>
+<node CREATED="1511631260502" ID="ID_1769713699" MODIFIED="1511631264442" TEXT="Szybka edycja">
+<node CREATED="1511631721810" ID="ID_328791390" MODIFIED="1511631728614" TEXT="Przeklejanie fragment&#xf3;w"/>
+<node CREATED="1511631737050" ID="ID_54099282" MODIFIED="1511631742061" TEXT="Napisanie jakiej&#x15b; funkcji"/>
+<node CREATED="1511631743650" ID="ID_1909175370" MODIFIED="1511631769942" TEXT="U&#x17c;ycie kropki do zmiany nazwy zmiennej"/>
+</node>
+<node CREATED="1511631267118" ID="ID_841747189" MODIFIED="1511631555870" TEXT="P&#xf3;j&#x15b;cie do definicji"/>
+<node CREATED="1511631557058" ID="ID_1575281170" MODIFIED="1511631564948" TEXT="znalezienie referencji"/>
+<node CREATED="1511631808034" ID="ID_655855200" MODIFIED="1511631817205" TEXT="skakanie mi&#x119;dzy plikami z ctrl P"/>
+<node CREATED="1511633389709" ID="ID_1742911313" MODIFIED="1511633400489" TEXT="W sumie to jest NeoVim">
+<node CREATED="1511633401725" ID="ID_1949262417" MODIFIED="1511633410449" TEXT="Ma troch&#x119; poprawek"/>
+<node CREATED="1511633411165" ID="ID_1260073830" MODIFIED="1511633418816" TEXT="Dost&#x119;p do wi&#x119;kszej ilo&#x15b;ci plugin&#xf3;w"/>
+<node CREATED="1511633421214" ID="ID_1999246490" MODIFIED="1511633431928" TEXT="&#x141;atwiej (niby) co&#x15b; dla niego napisa&#x107;"/>
+</node>
+</node>
+<node CREATED="1511631349408" ID="ID_1836335319" MODIFIED="1511631657950" TEXT="Tmux">
+<node CREATED="1511631659146" ID="ID_940963881" MODIFIED="1511631665534" TEXT="Dzielenie ekranu"/>
+<node CREATED="1511631666666" ID="ID_1584074807" MODIFIED="1511631682421" TEXT="Wiele okien"/>
+<node CREATED="1511631683290" ID="ID_1739443373" MODIFIED="1511631696398" TEXT="Przeklejanie tekstu mi&#x119;dzy Vimem, tmuxem, przegl&#x105;dark&#x105;"/>
+</node>
+<node CREATED="1511633701421" ID="ID_68156502" MODIFIED="1511633709489" TEXT="Ranger jako moje drzewo plik&#xf3;w"/>
+</node>
+<node CREATED="1511632128600" ID="ID_184661180" MODIFIED="1511632143227" POSITION="left" TEXT="Praca z Vimem (miejsce na drug&#x105; prezentacj&#x119;)">
+<node CREATED="1511632659906" ID="ID_1452760141" MODIFIED="1511632668550" TEXT="TODO">
+<node CREATED="1511633552386" ID="ID_1945955493" MODIFIED="1511633571443" TEXT="Problemy ze skakaniem do definicji async&#xf3;w?"/>
+<node CREATED="1511633571849" ID="ID_223596287" MODIFIED="1511633624939" TEXT="Znajdowanie referencji zmiennej w funkcji nie dzia&#x142;a, jak si&#x119; szuka od definicji parametru, dzia&#x142;a, jak si&#x119; sprawdzi od u&#x17c;ycia?"/>
+<node CREATED="1511633984448" ID="ID_430658996" MODIFIED="1511634003731" TEXT="Nie znajduje referencji do AppConfig w testach Iteriodata API - czemu?"/>
+<node CREATED="1511633634928" ID="ID_1552245134" MODIFIED="1511633649019" TEXT="Kolorowanie parametr&#xf3;w funkcji inaczej ni&#x17c; zmiennych w niej?"/>
+<node CREATED="1511634013679" ID="ID_837762378" MODIFIED="1511634032971" TEXT="Chodzenie do definicji funkcji i klas maj&#x105;cych dekorator"/>
+<node CREATED="1511632753857" ID="ID_1306740723" MODIFIED="1511632760989" TEXT="Sprawdzanie sk&#x142;adni"/>
+<node CREATED="1511633844899" ID="ID_908362152" MODIFIED="1511633871478" TEXT="Skakanie do definicji zmiennych w funkcji przez Jedi nie dzia&#x142;a, przez &quot;gd&quot; ju&#x17c; tak?"/>
+<node CREATED="1511632762153" ID="ID_652998232" MODIFIED="1511633061648" TEXT="odpalanie test&#xf3;w na skr&#xf3;t"/>
+<node CREATED="1511633122530" ID="ID_1379321369" MODIFIED="1511633140467" TEXT="Zmiana nazwy funkcji, zmiennej na skr&#xf3;t"/>
+<node CREATED="1511633141381" ID="ID_490712603" MODIFIED="1511633164224" TEXT="Przeniesienie funkcji/klasy do innego pliku (refactor)"/>
+<node CREATED="1511633446252" ID="ID_1973556808" MODIFIED="1511633452279" TEXT="Zast&#x119;pstwo na wielokursor">
+<node CREATED="1511633453884" ID="ID_916523911" MODIFIED="1511633461463" TEXT="Makra?"/>
+<node CREATED="1511633461979" ID="ID_379294980" MODIFIED="1511633491223" TEXT="c w jednym miejscu, potem skakanie do kolejnych wyst&#x105;pie&#x144; i kropka?"/>
+</node>
+<node CREATED="1511633500570" ID="ID_283020665" MODIFIED="1511633518214" TEXT="Automatyczna analiza statyczna pliku"/>
+<node CREATED="1511633666647" ID="ID_1487802052" MODIFIED="1511633679866" TEXT="Wykluczy&#x107; folder z szukania referencji (np. tox)"/>
+<node CREATED="1511633731845" ID="ID_1041259431" MODIFIED="1511633747161" TEXT="Sprawdzi&#x107; jak te problemy maj&#x105; si&#x119; do deoplete i neocomplete?"/>
+<node CREATED="1511633881937" ID="ID_1744155984" MODIFIED="1511633894126" TEXT="Automatyczne wstawianie dosctringa pod funkcj&#x105;?"/>
+<node CREATED="1511633894874" ID="ID_572303385" MODIFIED="1511633931437" TEXT="Odpowiednie prze&#x142;amywanie string&#xf3;w na enter? &#x17b;eby apostrofy/cudzys&#x142;owy si&#x119; dodawa&#x142;y"/>
+<node CREATED="1511633939313" ID="ID_1103862171" MODIFIED="1511633953141" TEXT="Spellcheck tylko dla plik&#xf3;w z kodem?"/>
+<node CREATED="1511633954008" ID="ID_663309308" MODIFIED="1511633967148" TEXT="Spellcheck w&#x142;&#x105;czany / wy&#x142;&#x105;czany na skr&#xf3;t"/>
+<node CREATED="1511634044231" ID="ID_121712228" MODIFIED="1511634062049" TEXT="Ranger i bash/fish z darkul&#x105; i 256 kolorami?"/>
+<node CREATED="1511634143237" ID="ID_1243334430" MODIFIED="1511634170480" TEXT="Przejd&#x17a; przez config i dodaj do &quot;do pokazania&quot; wszystkie tricki tam zawarte."/>
+</node>
+<node CREATED="1511632671562" ID="ID_1095140654" MODIFIED="1511632680718" TEXT="Do pokazania">
+<node CREATED="1511632683049" ID="ID_1756251469" MODIFIED="1511633068341" TEXT="znajdowanie referencji"/>
+<node CREATED="1511632695898" ID="ID_1104048035" MODIFIED="1511632713445" TEXT="przej&#x15b;cie do definicji"/>
+<node CREATED="1511632722138" ID="ID_783472620" MODIFIED="1511632729629" TEXT="bufory zamiast tab&#xf3;w">
+<node CREATED="1511632732433" ID="ID_1604373173" MODIFIED="1511632739781" TEXT="Ctrl P te&#x17c;">
+<node CREATED="1511633196275" ID="ID_1430939496" MODIFIED="1511633206470" TEXT="Przechodzenie do innych plik&#xf3;w"/>
+</node>
+</node>
+<node CREATED="1511633222570" ID="ID_1669191729" MODIFIED="1511633253310" TEXT="Wyszukanie po skopiowanym tek&#x15b;cie"/>
+<node CREATED="1511633822651" ID="ID_1975371174" MODIFIED="1511633831359" TEXT="[[ i ]] do skakania po definicjach"/>
+<node CREATED="1511634090734" ID="ID_330894033" MODIFIED="1511634099666" TEXT="Integracja ze schowkiem systemowym">
+<node CREATED="1511634195620" ID="ID_1307492488" MODIFIED="1511634211800" TEXT="Potrzebny NeoVim albo vim-gtk"/>
+</node>
+<node CREATED="1511634102750" ID="ID_233129788" MODIFIED="1511634123561" TEXT="Jest wiele bufor&#xf3;w do przeklejania/wycinania itp. Nie trzeba sobie nadpisywa&#x107; wszystkiego."/>
+<node CREATED="1511634136181" ID="ID_281637160" MODIFIED="1511634136181" TEXT=""/>
 </node>
 </node>
 </node>

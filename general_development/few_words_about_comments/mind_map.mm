721d7b1b72c3e13570cb71ede6c4588656d951ba
Michal Bultrowicz <michalbultrowicz@gmail.com>
2018-02-27 10:51:32 +0100

    A few words about code comments, and descriptions. In Polish.

---
+++ general_development/few_words_about_comments/mind_map.mm
@@ -0,0 +1,108 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1519565905942" ID="ID_207310093" MODIFIED="1519565935927" TEXT="Few words about comments, docs, messages">
+<node CREATED="1519565950538" ID="ID_1683105404" MODIFIED="1519573289538" POSITION="right" TEXT="Kod musi opowiada&#x107; histori&#x119; programi&#x15b;cie, kt&#xf3;ry na niego patrzy">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1519568288183" ID="ID_1013839940" MODIFIED="1519572454480" TEXT="Historia to">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1519568317024" ID="ID_278105474" MODIFIED="1519568318968" TEXT="Co robi"/>
+<node CREATED="1519568319454" ID="ID_1380002815" MODIFIED="1519568321483" TEXT="Dlaczego"/>
+<node CREATED="1519568321968" ID="ID_1367988248" MODIFIED="1519568332580" TEXT="Co ma robi&#x107; w przysz&#x142;o&#x15b;ci"/>
+</node>
+<node CREATED="1519566009661" ID="ID_1857289893" MODIFIED="1519572796349" TEXT="musi rozumie&#x107; o co chodzi na tyle, &#x17c;eby m&#xf3;c go zmienia&#x107;">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1519566034868" ID="ID_736160738" MODIFIED="1519723223686" TEXT="Nie mam twardych dowod&#xf3;w na to, &#x17c;e to, co robi&#x119; z kodem b&#x119;dzie si&#x119; skalowa&#x107; na wi&#x119;kszy zesp&#xf3;&#x142; (bo pracuj&#x119; w ma&#x142;ych), ale widz&#x119; po sobie, &#x17c;e dobrze mi si&#x119; porusza po kodzie i go zmienia, a te&#x17c; m&#xf3;j szef (poziom Pythona raczej junior) rozumie m&#xf3;j kod bez t&#x142;umaczenia i mo&#x17c;e sam go zmienia&#x107;.">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1519566096036" FOLDED="true" ID="ID_360810894" MODIFIED="1519723092358" TEXT="Ale ja, jak patrz&#x119; na kod miesi&#x105;c po jego napisaniu jestem troch&#x119; jak inna osoba, bo mam inny stan umys&#x142;u.">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1519568222003" ID="ID_106786688" MODIFIED="1519568252157" TEXT="Troch&#x119; jakby rozmawia&#x107; samemu ze sob&#x105;, tylko &#x17c;e faktycznie jeste&#x15b;my r&#xf3;&#x17c;nymi osobami"/>
+</node>
+<node CREATED="1519568486278" ID="ID_835709520" MODIFIED="1519573278786" TEXT="Maj&#x105;c to mo&#x17c;emy">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1519568498692" ID="ID_1488537339" MODIFIED="1519568510454" TEXT="Szybciej wr&#xf3;ci&#x107; do projektu po przerwie"/>
+<node CREATED="1519568511074" ID="ID_1538692501" MODIFIED="1519568720696" TEXT="Nieraz szybko rozwi&#x105;za&#x107; problemy, na kt&#xf3;rych rozwi&#x105;zanie wcze&#x15b;niej nie mogli&#x15b;my wpa&#x15b;&#x107;"/>
+</node>
+</node>
+<node CREATED="1519566165369" ID="ID_1667766325" MODIFIED="1519566178266" POSITION="left" TEXT="Co opr&#xf3;cz kodu opowiada histori&#x119;">
+<node CREATED="1519566182588" ID="ID_308920146" MODIFIED="1519566191152" TEXT="Komentarze">
+<node CREATED="1519566319173" ID="ID_1529265412" MODIFIED="1519573433851" TEXT="Dla innych programist&#xf3;w pracuj&#x105;cych nad tym kodem">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1519566390001" ID="ID_1551360904" MODIFIED="1519573443979" TEXT="Nie powinny m&#xf3;wi&#x107; co kod robi">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1519566411694" ID="ID_1484545219" MODIFIED="1519568089025" TEXT="Je&#x15b;li tak jest, to kod trzeba przerobi&#x107;, &#x17c;eby by&#x142; czystszy">
+<node CREATED="1519568094536" ID="ID_1363537039" MODIFIED="1519568135466" TEXT="Czasem nie da si&#x119; tego zrobi&#x107; ze wzgl&#x119;du na optymalizacj&#x119;, wtedy wypada napisa&#x107; komentarze m&#xf3;wi&#x105;ce o co chodzi"/>
+</node>
+</node>
+<node CREATED="1519566616116" ID="ID_720585477" MODIFIED="1519566627746" TEXT="Powinny m&#xf3;wi&#x107; o">
+<node CREATED="1519566631316" ID="ID_1616438931" MODIFIED="1519566635704" TEXT="Rozterkach">
+<node CREATED="1519567542776" ID="ID_726404396" MODIFIED="1519567558531" TEXT="Nie jeste&#x15b;my idealni i wszechwiedz&#x105;cy, nie ma co tego udawa&#x107;">
+<node CREATED="1519567560864" ID="ID_1199064093" MODIFIED="1519567577577" TEXT="Ale je&#x15b;li mo&#x17c;na co&#x15b; szybko sprawdzi&#x107;, to lepiej do od razu zrobi&#x107;"/>
+</node>
+</node>
+<node CREATED="1519566636157" ID="ID_565544517" MODIFIED="1519566641949" TEXT="Planach na przysz&#x142;o&#x15b;&#x107;"/>
+<node CREATED="1519566642471" ID="ID_109265813" MODIFIED="1519566653580" TEXT="Rzeczach, kt&#xf3;re jeszcze trzeba zrobi&#x107;">
+<node CREATED="1519566664062" ID="ID_1635702992" MODIFIED="1519568402266" TEXT="Kod czasem nie znajduje si&#x119; w stanie &quot;docelowej&quot; implementacji lub nie jest doko&#x144;czony">
+<node CREATED="1519566696288" ID="ID_168020387" MODIFIED="1519568425777" TEXT="Nie trzeba tego jednak traktowa&#x107; szczeg&#xf3;lnie i mo&#x17c;na to zasygnalizowa&#x107; innym programistom, lub sobie z przysz&#x142;o&#x15b;ci"/>
+</node>
+</node>
+<node CREATED="1519566502795" ID="ID_531535959" MODIFIED="1519566758008" TEXT="Dlaczego co&#x15b; jest zaimplementowane w ten, a nie inny spos&#xf3;b">
+<node CREATED="1519566762471" ID="ID_692826646" MODIFIED="1519566794114" TEXT="To mo&#x17c;e zmniejszy&#x107; szans&#x119; powracaj&#x105;cych k&#x142;&#xf3;tni technicznych"/>
+<node CREATED="1519566800855" ID="ID_1660959878" MODIFIED="1519566838287" TEXT="Kiedy trzyma si&#x119; te informacje w w&#x105;tkach mailowych albo w Jirze, to informacja mo&#x17c;e zanikn&#x105;&#x107;"/>
+</node>
+</node>
+<node CREATED="1519566515372" ID="ID_1486680569" MODIFIED="1519566528502" TEXT="Mo&#x17c;na wyrazi&#x107; w nich swoje rosterki">
+<node CREATED="1519566534851" ID="ID_737249461" MODIFIED="1519566544259" TEXT="Kod rzadko jest w idealnym stanie"/>
+</node>
+</node>
+<node CREATED="1519566191864" ID="ID_956685287" MODIFIED="1519568737574" TEXT="Docstringi/dokumentacja funkcji/klas/plik&#xf3;w">
+<node CREATED="1519566355074" ID="ID_319330274" MODIFIED="1519566371590" TEXT="Dla programist&#xf3;w u&#x17c;ywaj&#x105;cych naszego projektu">
+<node CREATED="1519567960064" ID="ID_984949537" MODIFIED="1519568019637" TEXT="Mog&#x105; miesza&#x107; rol&#x119; z komentarzami, bo jednak w przypadku du&#x17c;ej cz&#x119;&#x15b;ci wewn&#x119;trznego kodu programi&#x15b;ci s&#x105; zar&#xf3;wno u&#x17c;ytkownikami jak i wsp&#xf3;&#x142;tw&#xf3;rcami"/>
+</node>
+<node CREATED="1519566426465" ID="ID_1728675582" MODIFIED="1519568857196" TEXT="Powinny m&#xf3;wi&#x107;, co kod robi i jaka jest jego rola">
+<node CREATED="1519566436960" ID="ID_368991810" MODIFIED="1519566454474" TEXT="Je&#x15b;li robi&#x105; si&#x119; zbyt skomplikowane, to pewnie trzeba upro&#x15b;ci&#x107; kod"/>
+<node CREATED="1519566455468" ID="ID_853499615" MODIFIED="1519566483895" TEXT="Ich pisanie pomaga przemy&#x15b;le&#x107; kod i wychwyci&#x107; z&#x142;y design (rubber duck)"/>
+</node>
+<node CREATED="1519568751085" ID="ID_313234841" MODIFIED="1519568788482" TEXT="W przypadku plik&#xf3;w mog&#x105; t&#x142;umaczy&#x107;, dlaczego znajduje si&#x119; on akurat w takim miejscu w strukturze projektu"/>
+<node CREATED="1519568884418" ID="ID_522890546" MODIFIED="1519568947536" TEXT="kod, dosctringi, komentarze powinny same m&#xf3;wi&#x107; to, co by&#x15b; powiedzia&#x142; t&#x142;umacz&#x105;c si&#x119; ze swojego kodu komu&#x15b; innemu"/>
+</node>
+<node CREATED="1519566217674" ID="ID_1789472523" MODIFIED="1519566233248" TEXT="Wiadomo&#x15b;ci w Gicie">
+<node CREATED="1519566873220" ID="ID_1484659181" MODIFIED="1519566883441" TEXT="Lepiej trzyma&#x107; informacje o kodzie w kodzie"/>
+<node CREATED="1519566883775" ID="ID_394323577" MODIFIED="1519566902110" TEXT="Powinny m&#xf3;wi&#x107; o tym, co si&#x119; dzieje z projektem">
+<node CREATED="1519566918914" ID="ID_926017214" MODIFIED="1519566927520" TEXT="Dlaczego dane zmiany by&#x142;y wprowadzone">
+<node CREATED="1519566935113" ID="ID_1754014132" MODIFIED="1519566948517" TEXT="TODO zobacz sobie log kernela linuxa"/>
+</node>
+<node CREATED="1519566957684" ID="ID_1477187564" MODIFIED="1519566984335" TEXT="co dalej b&#x119;dzie zrobione w zwi&#x105;zku z tymi zmianami">
+<node CREATED="1519566986896" ID="ID_135195051" MODIFIED="1519566994544" TEXT="Dobre przy trunk-based"/>
+</node>
+</node>
+</node>
+<node CREATED="1519566240417" ID="ID_1725158015" MODIFIED="1519566267857" TEXT="Zadania w systemie w stylu Jiry/Trello">
+<node CREATED="1519566846849" ID="ID_1797559070" MODIFIED="1519566868923" TEXT="Gadanie o kodzie tutaj zamiast w kodzie/repozytorium prowadzi do zapomnienia"/>
+<node CREATED="1519567106628" ID="ID_1153692897" MODIFIED="1519567129383" TEXT="Lepiej trzyma&#x107; info o projekcie we wiadomo&#x15b;ciach Gita">
+<node CREATED="1519567154412" ID="ID_505333445" MODIFIED="1519567166325" TEXT="Managerowie musz&#x105; si&#x119; nauczy&#x107; korzysta&#x107; z Gita">
+<node CREATED="1519567176875" ID="ID_1843429929" MODIFIED="1519567186027" TEXT="Albo w og&#xf3;le przesta&#x107; istnie&#x107; jako tacy"/>
+</node>
+</node>
+<node CREATED="1519567201080" ID="ID_350938335" MODIFIED="1519567212357" TEXT="Przydaj&#x105; si&#x119; do">
+<node CREATED="1519567215148" ID="ID_212958743" MODIFIED="1519567219717" TEXT="Kolejkowania zada&#x144;"/>
+<node CREATED="1519567220194" ID="ID_1878364781" MODIFIED="1519567244191" TEXT="Wst&#x119;pnej dyskusji na temat zakresu zada&#x144; i ich implementacji">
+<node CREATED="1519567247892" ID="ID_1676879097" MODIFIED="1519567350137" TEXT="Mo&#x17c;e si&#x119; okaza&#x107;, &#x17c;e zadanie jest zb&#x119;dne albo mo&#x17c;e by&#x107; bardzo ograniczone. To jest bardzo dobre, bo pisanie jakiegokolwiek kodu to zaci&#x105;ganie d&#x142;ugu technicznego"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1519567471534" ID="ID_1652923787" MODIFIED="1519573297425" POSITION="right" TEXT="Kod i te dodatkowe rzeczy to jedna ca&#x142;o&#x15b;&#x107;">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1519567592185" ID="ID_681651752" MODIFIED="1519567646551" POSITION="left" TEXT="pisanie tekstu ne temat kodu pomaga uporz&#x105;dkowa&#x107; my&#x15b;li na jego temat">
+<node CREATED="1519567654336" ID="ID_1324490033" MODIFIED="1519567670553" TEXT="To tak dzia&#x142;a og&#xf3;lnie, nie tylko do kodu"/>
+<node CREATED="1519567673199" ID="ID_580599844" MODIFIED="1519567730771" TEXT="przez to mog&#x105; pojawi&#x107; si&#x119; nowe wnioski na temat kodu">
+<node CREATED="1519567746570" ID="ID_342018891" MODIFIED="1519567781030" TEXT="Np. &#x17c;e co&#x15b; mo&#x17c;na inaczej zaprojektowa&#x107;, albo &#x17c;e jest zb&#x119;dne"/>
+</node>
+</node>
+</node>
+</map>

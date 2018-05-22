96df667a1e5350c47edf2fd3e8b0d9eb89b78bce
Michal Bultrowicz <michalbultrowicz@gmail.com>
2018-05-22 22:30:57 +0200

    Asyncio in action: possible example file.

---
+++ python/asyncio_in_action/when_tasks_are_running.py
@@ -0,0 +1,43 @@
+import asyncio
+import time
+
+
+async def backgroud_work():
+    print('entering sync sleep')
+    time.sleep(2)
+    print('entering async sleep')
+    await asyncio.sleep(2)
+    print('main task done')
+
+
+async def backgroud_work_secondary():
+    await asyncio.sleep(1)
+    print('background 1!')
+
+
+async def backgroud_work_tertiary():
+    print("I'm executed when I'm created until the first await. Before that I'm sync.")
+
+    # Why does this run if the main task only sleeps asynchronously for 2 seconds?
+    # because I count wait time from task creation (but does it get to first
+    # await?
+    await asyncio.sleep(3)
+    print('background 2!')
+
+
+# don't know the word for fourth in that form
+async def backgroud_work_last():
+    # I won't be executed, because I wait longer that the main tasks waits synchronously
+    # and asynchronously combined!
+    await asyncio.sleep(4.5)
+    print('background 3!')
+
+
+l = asyncio.get_event_loop()
+l.create_task(backgroud_work_secondary())
+l.create_task(backgroud_work_tertiary())
+
+l.run_until_complete(backgroud_work())
+print('ended')
+time.sleep(2)
+print("the last task won't be executed, because execution isn't in the loop anymore.")
